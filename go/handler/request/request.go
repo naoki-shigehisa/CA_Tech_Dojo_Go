47e72f7c4333382a21ef940f3e93b552dcbfd1cb
naoki-shigehisa <naoki.shigehisa@gmail.com>
2021-05-10 23:12:01 +0900

    [fix]handler/request,handler/response 構造体定義

--- go/handler/request/request.go
+++ go/handler/request/request.go
@@ -1,1 +1,17 @@
 package handler
+
+// ユーザー作成用
+type UserCreateRequest struct {
+	Name       string `json:"name"`
+}
+
+// ユーザー取得用
+type UserGetRequest struct {
+	Token       string `json:"x-token"`
+}
+
+// ユーザー作成用
+type UserUpdateRequest struct {
+	Token       string `json:"x-token"`
+	Name       string `json:"name"`
+}
--- go/handler/response/response.go
+++ go/handler/response/response.go
@@ -1,1 +1,16 @@
 package handler
+
+// ユーザー作成用
+type UserCreateResponse struct {
+	Token       string `json:"token"`
+}
+
+// ユーザー取得用
+type UserGetResponse struct {
+	Name       string `json:"name"`
+}
+
+// その他エラー等
+type Response struct {
+	Status       string `json:"status"`
+}
